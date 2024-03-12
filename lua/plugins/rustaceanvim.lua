f5f4ac32e265a42059c3566fa4e89d4303e2e9b6
lemonxah <lemon@xahnet.net>
2024-03-12 21:00:23 +0200

    auto enable inlays

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -3,5 +3,28 @@ return {
     "mrcjkb/rustaceanvim",
     version = "^4", -- Recommended
     ft = { "rust" },
+    ---@type RustaceanOpts
+    opts = {
+      ---@type RustaceanLspClientOpts
+      server = {
+        default_settings = {
+          ["rust-analyzer"] = {
+            files = {
+              excludeDirs = { ".direnv", "result" },
+            },
+            imports = {
+              granularity = { group = "module" },
+              prefix = "self",
+              prefer_prelude = true,
+            },
+          },
+        },
+        on_attach = function(client, bufnr)
+          if client.name == "rust-analyzer" and vim.fn.has("nvim-0.10") then
+            vim.lsp.inlay_hint.enable(bufnr, true)
+          end
+        end,
+      },
+    },
   },
 }
